b778a376ecd1e26e6a42afd32a4fd2d87acb834c
sanm1992 <1320695403@qq.com>
2020-10-10 10:21:50 +0800

    修改默认词库路径

--- lib/sensitive_word_filter.rb
+++ lib/sensitive_word_filter.rb
@@ -14,7 +14,7 @@ module SensitiveWordFilter
 
     def initialize(file_paths=[])
       if file_paths.empty?
-        @words = Load.new('lib/sensitive_word_filter/words/广告.text').load
+        @words = Load.new("#{__dir__}/sensitive_word_filter/words/广告.text").load
       else
         @words = Load.new(file_paths).load
       end
